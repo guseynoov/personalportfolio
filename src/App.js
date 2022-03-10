f8353b9f8661670fdcca4a408508874f639cebd2
guseynoov <kanan.huseynov.2017@gmail.com>
2022-03-10 14:32:41 +0300

    Son merhele

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header';
+import Contactme from './components/Contactme';
 function App() {
-  return (
-	  <>
-   <Navbar />
+	return (
+		<>
+			<Navbar />
 
-	<Header/>
-	</>
-  );
+			<Header />
+
+			<Contactme />
+		</>
+
+	);
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
 import shekil from '../wall-and-laptop-background.jpeg'
+import Typed from 'react-typed';
 
 const Header = () => {
   return (
 	 <div className="header-wrapper">
 	 	<div className="main-info">
 		  <h1>Web development and website services</h1>
+
+		  <Typed
+		  className='typed-text'
+                strings={[
+                    'DIA student',
+                    'Computer Science',
+                    'Front-end Developer']}
+                    typeSpeed={40}
+                    backSpeed={50} 
+                    loop >
+                  
+                </Typed>
  <a href={shekil} className="downloadcv" download>Download CV</a>
 	 </div>
 	 
